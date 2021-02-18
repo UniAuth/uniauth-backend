857b77b828e1d890439028d21f050881332396f9
Harsh Pandey <harshpandey011@gmail.com>
2021-02-18 09:12:26 +0530

    Added tests for mailer service

--- src/mailer/mailer.service.spec.ts
+++ src/mailer/mailer.service.spec.ts
@@ -1,18 +1,81 @@
+import { JwtModule } from '@nestjs/jwt';
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { User, UserDocument, UserSchema } from 'src/user/user.schema';
+import { rootMongooseTestModule } from 'test-utils/MongooseTestModule';
+import { confirmEmailTokenConstants } from './constants/confirmEmailToken.constants';
 import { MailerService } from './mailer.service';
 
+const mockUser = (mock?: Partial<User>): Partial<UserDocument> => ({
+  name: mock.name || 'some user',
+  batch: mock.batch || '19',
+  branch: mock.branch || 'BCE',
+  personalEmail: mock.personalEmail || 'someone@example.com',
+  collegeEmail: mock.collegeEmail || 'someoe@edu.in',
+});
+
 describe('MailerService', () => {
+  let testingModule: TestingModule;
   let service: MailerService;
+  let model: Model<UserDocument>;
+
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MailerService],
+    testingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        JwtModule.register({
+          secret: confirmEmailTokenConstants.secret,
+          signOptions: { expiresIn: confirmEmailTokenConstants.expiresIn },
+        }),
+      ],
+      providers: [MailerService,
+        {
+        provide: getModelToken(User.name),
+        useValue: {
+          findByIdAndUpdate: jest.fn().mockResolvedValue(mockUser()),
+        },
+      },],
     }).compile();
 
-    service = module.get<MailerService>(MailerService);
+    service = testingModule.get<MailerService>(MailerService);
+    model = testingModule.get<Model<UserDocument>>(getModelToken(User.name));
+
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('.generateJwt()', () => {
+    it('should be defined', () => {
+      expect(service.generateJwt).toBeDefined();
+    });
+  });
+
+  describe('.checkVerificationToken()', () => {
+    it('should be defined', () => {
+      expect(service.checkVerificationToken).toBeDefined();
+    });
+  });
+
+  describe('.checkPasswordResetToken()', () => {
+    it('should be defined', () => {
+      expect(service.checkPasswordResetToken).toBeDefined();
+    });
+  });
+  
+  describe('.sendEmail()', () => {
+    it('should be defined', () => {
+      expect(service.sendEmail).toBeDefined();
+    });
+  });
+
+  describe('.sendPasswordResetLink()', () => {
+    it('should be defined', () => {
+      expect(service.sendPasswordResetLink).toBeDefined();
+    });
   });
 });
