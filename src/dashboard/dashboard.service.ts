f38bdffc5c7588a5acde26080d5c254532ec8cc7
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-02-21 16:28:51 +0530

    Update dashboard.service.ts

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-// import { CreateDashboardDto } from './dto/create-dashboard.dto';
 import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 
 @Injectable()
 export class DashboardService {
-  // create(createDashboardDto: CreateDashboardDto) {
-  //   return 'This action adds a new dashboard';
-  // }
 
   findAll() {
     return `This action returns all dashboard`;
@@ -16,9 +12,6 @@ export class DashboardService {
     return `This action returns a #${id} dashboard`;
   }
 
-  // update(id: number, updateDashboardDto: UpdateDashboardDto) {
-  //   return `This action updates a #${id} dashboard`;
-  // }
 
   remove(id: number) {
     return `This action removes a #${id} dashboard`;
