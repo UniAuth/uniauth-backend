f68ff2326afd6463895e395f2664056e9ca5a67a
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-02-25 01:23:34 +0530

    Update dashboard.controller.ts

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -91,18 +91,17 @@ export class DashboardController {
 /**
  * To load edit form
  */
-//   @todo: migrate to dashboard
-//   @Get('/:id/edit')
-//   @UseGuards(JwtAuthGuard)
-//   async showEditForm(@Res() res: Response, @Param('id') id: string) {
-//     const user = await this.userService.findOneById(id);
-//     return res.render('profile/edit.hbs', { user });
-//   }
+  @Get('/:id/edit')
+  @UseGuards(JwtAuthGuard)
+  async showEditForm(@Res() res: Response, @Param('id') id: string) {
+    const user = await this.userService.findOneById(id);
+    return res.render('profile/edit.hbs', { user });
+  }
 
-//   @Post('/:id/edit')
-//   @UseGuards(JwtAuthGuard)
-//   async PostEditForm(
-//     @Res() res: Response,
-//   ) {
-//     return res.redirect('/dashboard/');
-//   }
+  @Post('/:id/edit')
+  @UseGuards(JwtAuthGuard)
+  async PostEditForm(
+    @Res() res: Response,
+  ) {
+    return res.redirect('/dashboard/');
+  }
