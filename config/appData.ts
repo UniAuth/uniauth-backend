ce5b41e7f0d6d142ce3b65a740bbe949c74ed962
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-02-28 18:08:34 +0530

    Delete appData.ts

--- config/appData.ts
+++
@@ -1,7 +0,0 @@
-import * as config from 'config';
-
-export const appData = {
-  Name: config.get('api.name'),
-  Description: config.get('api.description'),
-  Version: config.get('api.version'),
-};
